3ab46d596b85cbd4245cca4bf46e5003de10dc0a
Liyu <wangliyu@huoxing24.com>
2022-06-01 17:42:09 +0800

    frist

---
+++ index.ts
@@ -0,0 +1,33 @@
+export {
+    Web3Accounts,
+    assetToMetadata,
+    metadataToAsset,
+    tokenToAsset,
+    tokenToMetadata,
+    transactionToCallData,
+    ETHToken,
+    NullToken
+} from './src'
+
+export type {
+    APIConfig,
+    ExchangetAgent,
+    Asset,
+    MetaAsset,
+    ExchangeMetadata,
+    Token,
+    MixedPayment,
+    MatchParams,
+    BuyOrderParams,
+    SellOrderParams,
+    CreateOrderParams,
+    LowerPriceOrderParams,
+    AcceptOrderOption,
+    BatchAcceptOrderOption
+} from './src/types'
+
+export {TokenSchemaName, ElementSchemaName, OrderType, OfferType} from './src/types'
+
+
+
+
---
+++ test/signTypeData.ts
@@ -0,0 +1,116 @@
+import {Asset, EIP712TypedData, getEstimateGas, Web3Accounts} from "../index"
+// @ts-ignore
+import * as secrets from '../../../secrets.json'
+import {getEIP712Hash} from "web3-wallets";
+
+
+const data = {
+    "types": {
+        "EIP712Domain": [],
+        "ERC721Order": [
+            {
+                "type": "uint8",
+                "name": "direction"
+            },
+            {
+                "type": "address",
+                "name": "maker"
+            },
+            {
+                "type": "address",
+                "name": "taker"
+            },
+            {
+                "type": "uint256",
+                "name": "expiry"
+            },
+            {
+                "type": "uint256",
+                "name": "nonce"
+            },
+            {
+                "type": "address",
+                "name": "erc20Token"
+            },
+            {
+                "type": "uint256",
+                "name": "erc20TokenAmount"
+            },
+            {
+                "type": "Fee[]",
+                "name": "fees"
+            },
+            {
+                "type": "address",
+                "name": "erc721Token"
+            },
+            {
+                "type": "uint256",
+                "name": "erc721TokenId"
+            },
+            {
+                "type": "Property[]",
+                "name": "erc721TokenProperties"
+            }
+        ],
+        "Fee": [
+            {
+                "type": "address",
+                "name": "recipient"
+            },
+            {
+                "type": "uint256",
+                "name": "amount"
+            },
+            {
+                "type": "bytes",
+                "name": "feeData"
+            }
+        ],
+        "Property": [
+            {
+                "type": "address",
+                "name": "propertyValidator"
+            },
+            {
+                "type": "bytes",
+                "name": "propertyData"
+            }
+        ]
+    },
+    "domain": {
+        "chainId": 1,
+        "verifyingContract": "0xdef1c0ded9bec7f1a1670819833240f027b25eff",
+        "name": "ZeroEx",
+        "version": "1.0.0"
+    },
+    "primaryType": "ERC721Order",
+    "message": {
+        "direction": 0,
+        "maker": "0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
+        "taker": "0x0000000000000000000000000000000000000000",
+        "expiry": "86410",
+        "nonce": "10",
+        "erc20Token": "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+        "erc20TokenAmount": "1",
+        "fees": [],
+        "erc721Token": "0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
+        "erc721TokenId": "2",
+        "erc721TokenProperties": []
+    }
+}
+const seller = '0x9F7A946d935c8Efc7A8329C0d894A69bA241345A';
+(async () => {
+    const chainId = 97
+    const user = new Web3Accounts({
+        chainId,
+        address: seller,
+        priKey: secrets.accounts[seller]
+    })
+
+    // const signMsg = await user.signMessage("hello")
+    // const hash = getEIP712Hash(data)
+    // console.log(signMsg)
+    const sign = await user.signTypedData(data)
+    console.log(sign)
+})()
